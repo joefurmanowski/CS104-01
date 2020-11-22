bbe72329a08f528b92f6fbd6f3dcbbe4e2179985
joefurmanowski <71726037+joefurmanowski@users.noreply.github.com>
2020-11-22 12:55:36 -0500

    Second version of the customized Python queue simulation program

--- CustomPythonQueue.py
+++ CustomPythonQueue.py
@@ -5,10 +5,10 @@ names = []
 numberOfNames = int(input("How many people are in the queue?: "))
 
 # For loop asking for numberOfNames individual names and appends each to the names list
-for i in range(0,numberOfNames):
+for i in range(0, numberOfNames):
     acceptedName = str(input("Please enter name {}/{}: ".format(i+1, numberOfNames)))
     names.append(acceptedName)
 
 # Second for loop that deques/pops each name and prints each
-for j in range(len(names)):
+for j in range(0, len(names)):
     print(names.pop(0))
